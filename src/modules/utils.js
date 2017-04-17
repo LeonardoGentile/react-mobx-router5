c64cc007c41257dc81006fd47403651199e0bc19
Leonardo Gentile <leonard.gentile@gmail.com>
2017-04-17 11:28:21 +0200

    getComponentFromRoutes changes

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -8,25 +8,25 @@ export const ifNot = (condition, errorMessage) => {
 /**
  * Return a component to be rendered by a routeNode for the current route
  * extracted from the custom (nested) routes definition (with 'component' field)
- * @param {array} routes - nested routes def (with 'component' field)
- * @param {object} routerStore - the mobx-router store. Used for getting the current route
- * @param {string} routeNode - the name of the route for the React component from where to re-render (transition node)
+ * @param {object} route - the current active route object
+ * @param {string} routeNodeName - the name of the route for the React component from where to re-render (transition node)
+ * @param {array} routesConfig - nested routes configuration object (with 'component' field)
  * @returns {React.Component} - the component to be rendered
  */
-export function getComponentFromRoutes(routes, routerStore, routeNode) {
+export function getComponentFromRoutes(route, routeNodeName, routesConfig) {
   let level = 0;
   let currentLevel = 0;
   // Break current route in segments (levels)
-  const routeSegments = routerStore.route.name.split('.');
+  const routeSegments = route.name.split('.');
 
   // Set nesting level where to find routes for this routeNode
-  if (routeNode === '') level = 0;
-  else level = routeNode.split('.').length;
-  if (level === 0 && routeNode !== '') level = 1;
+  if (routeNodeName === '') level = 0;
+  else level = routeNodeName.split('.').length;
+  if (level === 0 && routeNodeName !== '') level = 1;
 
   // Recurse until it gets the relevant portion of the routes obj or the component itself
-  function getComponent(routesObj) {
-    for (const route of routesObj) {
+  function getComponent(routesConfig) {
+    for (const route of routesConfig) {
       const segment = routeSegments[currentLevel]; // going deeper every recursion
       if (route.name === segment) {
 
@@ -41,5 +41,5 @@ export function getComponentFromRoutes(routes, routerStore, routeNode) {
     }
   }
 
-  return getComponent(routes);
+  return getComponent(routesConfig);
 }
