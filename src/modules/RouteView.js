4942a87d59d25e6fe7696c41d343eb0d64e8c6a7
Leonardo Gentile <leonard.gentile@gmail.com>
2017-10-06 18:23:54 +0200

    feat(RouteView): Add support for React 16 and Error Boundaries
    Wrap RouteView into an Error Boundary component + update peer Dependencies
    
    #3

--- src/modules/RouteView.js
+++ src/modules/RouteView.js
@@ -1,6 +1,8 @@
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import {createElement} from 'react';
 import {getComponent} from './utils';
+
 
 /**
  * RouteView component: it should be used inside a routeNode component
@@ -10,30 +12,16 @@ import {getComponent} from './utils';
  * @return {object|null}
  */
 function RouteView(props) {
-
   const {route, routeNodeName, routes, ...passThroughProps } = props;
-
   let currentRoute = route;
-  let ComponentToRender = null;
 
-  try {
-    if (!currentRoute) {
-      throw new Error('RouteView component requires a route prop');
-    }
-    const FetchedComponent = getComponent(currentRoute, routeNodeName, routes); // getComponent may throw
-    // Add `{key: route.meta.id}` to props for a full unmount/mount
-    ComponentToRender = createElement(FetchedComponent, {...passThroughProps, route});
+  if (!currentRoute) {
+    throw new Error('RouteView component requires a route prop');
   }
-  catch (e) {
-    // Do not render and print error to console
-    console.error(`RouteView: it was not possible to select the correct view for the current route '${currentRoute.name}' having params: `);
-    // This outputs an object on the browser console you can click through
-    console.dir(currentRoute.params);
-  }
-
-  return ComponentToRender;
+  const FetchedComponent = getComponent(currentRoute, routeNodeName, routes); // getComponent may throw
+  // Add `{key: route.meta.id}` to props for a full unmount/mount
+  return createElement(FetchedComponent, {...passThroughProps, route});
 }
-
 
 RouteView.displayName = 'RouteView';
 
@@ -43,6 +31,49 @@ RouteView.propTypes = {
   route: PropTypes.object.isRequired
 };
 
-export default RouteView;
+
+
+
+class ErrorBoundRouteView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props !== nextProps) {
+      this.setState({ hasError: false });
+    }
+  }
+
+  componentDidCatch(error, info) {
+    const currentRoute = this.props.route;
+    // Display fallback UI
+    this.setState({ hasError: true });
+
+    console.error(`RouteView: it was not possible to select the correct view for the current route '${currentRoute.name}' having params: `);
+    // This outputs an object on the browser console you can click through
+    console.dir(currentRoute.params);
+    console.log(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>{this.props.errorMessage || 'Something went wrong.'}</h1>;
+    }
+    const {errorMessage, ...passThroughProps } = this.props;
+    return <RouteView {...passThroughProps} />
+  }
+}
+
+ErrorBoundRouteView.propTypes = {
+  routes: PropTypes.array.isRequired,
+  routeNodeName: PropTypes.string.isRequired,
+  route: PropTypes.object.isRequired,
+  errorMessage: PropTypes.string
+};
+
+
+export default ErrorBoundRouteView;
 
 
